f81bb3c2623bbdfe59b661cab7302dbea33190f2
stevenkeezer <stevengkeezer@gmail.com>
2023-01-16 20:34:15 -0800

    test: deploy

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
-import Head from 'next/head'
-import { AppProps } from 'next/app'
-import '../styles/index.css'
+import Head from "next/head";
+import { AppProps } from "next/app";
+import "../styles/index.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -9,9 +9,10 @@ function MyApp({ Component, pageProps }: AppProps) {
         <title>NextJS TailwindCSS TypeScript Starter</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
+      hi
       <Component {...pageProps} />
     </>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
